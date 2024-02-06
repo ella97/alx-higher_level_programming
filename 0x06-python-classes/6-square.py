17b5d244357ecbd1f23611b858534d0e65308501
ella97 <nuwymwanri@gmail.com>
2024-02-06 13:43:12 +0300

    .\6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,17 +3,21 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size):
-        """Initialize a new Square.
+    def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
 
         Args:
             size (int): The size of the new square.
+            position (int, int): The position of the new square.
         """
-        self.__size = size
+        self.size = size
+        self.position = position
+
     @property
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -21,10 +25,12 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     @property
     def position(self):
         """Get/set the current position of the square."""
         return (self.__position)
+
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
@@ -33,9 +39,11 @@ class Square:
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
+
     def my_print(self):
         """Print the square with the # character."""
         if self.__size == 0:
