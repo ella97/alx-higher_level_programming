4f45f8fe64c88bc9fc29b74f0afea3724d4a41f2
ella97 <nuwymwanri@gmail.com>
2024-05-24 15:30:52 +0300

    Changelog: New Corrections

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Get the byte size of the HTTP response header for the given URL.
-curl -s "$1" | wc -c
+# curl -s "$1" | wc -c
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f 2
